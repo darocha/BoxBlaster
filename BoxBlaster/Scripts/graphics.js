6edd306057ee954cb430dbf495ba77953d61cf68
Troy Whorten <sabotuer99@gmail.com>
2014-11-28 21:18:49 -0700

    added pewpew

--- BoxBlaster/Scripts/graphics.js
+++ BoxBlaster/Scripts/graphics.js
@@ -8,6 +8,54 @@
 
     //window.onresize = windowresize;
 
+
+    var Point = (function () {
+        function Point(x, y) {
+            this.x = x || 0;
+            this.y = y || 0;
+        }
+
+        return Point;
+    })();
+
+    var LineSegment = (function () {
+        function LineSegment(point1, point2) {
+            this.Point1 = point1 || new Point();
+            this.Point2 = point2 || new Point();
+        }
+
+        return LineSegment;
+    })();
+
+    var Vector = (function () {
+        function Vector(mag, dir) {
+            this.magnitude = mag || 0;
+            this.direction = dir || 0;
+
+            this.SetDirectionFromPoints = function (origin, target) {
+                if (origin instanceof Point && target instanceof Point) {
+                    var deltaY = target.y - origin.y;
+                    var deltaX = target.x - origin.x;
+                    this.direction = Math.atan2(deltaY, deltaX);
+
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            this.GetRelativeEndPoint = function () {
+                var endpoint = new Point();
+                endpoint.y += this.magnitude * Math.sin(this.direction);
+                endpoint.x += this.magnitude * Math.cos(this.direction);
+                return endpoint;
+            }
+        }
+
+        return Vector;
+    })();
+
+
     var Box = (function () {
         function Box() {
             this.id = Math.floor((1 + Math.random()) * 0x10000).toString(16);
@@ -17,8 +65,8 @@
             this.width = 30;
             this.height = 30;
             this.playPew = false;
-            this.aim_x;
-            this.aim_y;
+            this.aim_x = 0;
+            this.aim_y = 0;
             this.move_N = false,
             this.move_S = false,
             this.move_W = false,
@@ -43,12 +91,115 @@
                 ctx.fillText(this.name, this.x - this.width/2, this.y - this.height / 4, this.width);
 
             };
+
+            this.GetLineSegments = function () {
+                var topY = this.y - this.height / 2;
+                var botY = this.y + this.height / 2;
+                var leftX = this.x - this.width / 2;
+                var rightX = this.x + this.width / 2;
+
+                var topleft = new Point(leftX, topY);
+                var topright = new Point(rightX, topY);
+                var botleft = new Point(leftX, botY);
+                var botright = new Point(rightX, botY);
+
+                var segments = new Array();
+                segments.push(new LineSegment(topleft, botleft)); // left side
+                segments.push(new LineSegment(topleft, topright)); // top side
+                segments.push(new LineSegment(topright, botright)); // right side
+                segments.push(new LineSegment(botleft, botright)); // bottom side
+
+                return segments;
+            }
+
+            this.MoveMe = function () {
+                if (this.move_N)
+                    this.y -= 5;
+                if (this.move_S)
+                    this.y += 5;
+                if (this.move_W)
+                    this.x -= 5;
+                if (this.move_E)
+                    this.x += 5;
+            };
+
+            this.firePew = function () {
+                var vector = new Vector(15,0);
+                vector.SetDirectionFromPoints(new Point(this.x, this.y), new Point(this.aim_x, this.aim_y));
+                var pewBox = new PewPew(vector);
+                pewBox.id = this.id;
+                pewBox.x = this.x;
+                pewBox.y = this.y;
+                console.log(pewBox);
+                console.log(pewBox.vector);
+                pewBox.MoveMe();
+                console.log(pewBox);
+                PewPews.push(pewBox);
+            };
+
         }
 
         return Box;
     })();
 
+
+    var PewPew = (function () {
+        function PewPew(vector) {
+            this.id = Math.floor((1 + Math.random()) * 0x10000).toString(16);
+            this.x = 0;
+            this.y = 0;
+            this.width = 4;
+            this.height = 4;
+            this.color = "black";
+            this.vector = vector || new Vector();
+            this.DrawToCanvasContext = function (ctx) {
+                //draw rectangle
+                ctx.fillStyle = this.color;
+                ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+
+            };
+
+            this.GetLineSegments = function () {
+                var topY = this.y - this.height / 2;
+                var botY = this.y + this.height / 2;
+                var leftX = this.x - this.width / 2;
+                var rightX = this.x + this.width / 2;
+
+                var topleft = new Point(leftX, topY);
+                var topright = new Point(rightX, topY);
+                var botleft = new Point(leftX, botY);
+                var botright = new Point(rightX, botY);
+                var center = new Point(this.x, this.y);               
+                var tailrel = this.vector.GetRelativeEndPoint();
+                var tail = new Point(this.x - tailrel.x, this.y - tailrel.y);
+
+                var segments = new Array();
+                segments.push(new LineSegment(topleft, botleft)); // left side
+                segments.push(new LineSegment(topleft, topright)); // top side
+                segments.push(new LineSegment(topright, botright)); // right side
+                segments.push(new LineSegment(botleft, botright)); // bottom side
+                segments.push(new LineSegment(center, tail));
+
+                return segments;
+            }
+
+            this.MoveMe = function () {
+                var nextxy = this.vector.GetRelativeEndPoint();
+                this.y += nextxy.y;
+                this.x += nextxy.x;
+                //this.y += this.vector.magnitude * Math.sin(this.vector.direction);
+                //this.x += this.vector.magnitude * Math.cos(this.vector.direction);
+                //console.log(this);
+            };
+
+        }
+
+        return PewPew;
+    })();
+
+
     var Boxes = new Array();
+    var PewPews = new Array();
 
     var counter = 0;
     var lastMessageTime = new Date();
@@ -99,16 +250,17 @@
         if (now - lastMessageTime >= 40) {
             lastMessageTime = now;
             counter++;
+
+            var angle = 
+
+            //console.log("AimMsg#" + counter + " x: " + event.clientX + " y: " + event.clientY);
             console.log("AimMsg#" + counter + " x: " + event.clientX + " y: " + event.clientY);
             MyBox.aim_x = event.clientX;
             MyBox.aim_y = event.clientY;
-            //Boxes.forEach(function (item) {
-            //    item.aim_x = event.clientX;
-            //    item.aim_y = event.clientY;
-            //});
-            //render();
-
-            
+            Boxes.forEach(function (item) {
+                item.aim_x = event.clientX;
+                item.aim_y = event.clientY;
+            });                 
         }
     }
 
@@ -173,14 +325,20 @@
     }
 
     function movebox() {
-        if (MyBox.move_N)
-            MyBox.y -= 5;
-        if (MyBox.move_S)
-            MyBox.y += 5;
-        if (MyBox.move_W)
-            MyBox.x -= 5;
-        if (MyBox.move_E)
-            MyBox.x += 5;
+
+        MyBox.MoveMe();
+
+        PewPews.forEach(function (pew) {
+            pew.MoveMe();
+        });
+        //if (MyBox.move_N)
+         //   MyBox.y -= 5;
+        //if (MyBox.move_S)
+          //  MyBox.y += 5;
+        //if (MyBox.move_W)
+          //  MyBox.x -= 5;
+        //if (MyBox.move_E)
+          //  MyBox.x += 5;
     }
 
     function mouseclick(event) {
@@ -214,6 +372,11 @@
         Boxes.forEach(function (item) {
             item.DrawToCanvasContext(tctx);
         });
+        PewPews.forEach(function (item) {
+            item.DrawToCanvasContext(tctx);
+        });
+       
+
         MyBox.DrawToCanvasContext(tctx);
 
         //draw tempCanvas to drawCanvas
@@ -229,8 +392,12 @@
             var dist = Math.sqrt(Math.pow(MyBox.x, 2) + Math.pow(MyBox.y, 2));
             var pew = new Audio("Audio/pew.wav");
             pew.volume = (maxDist - dist) / maxDist;
-            pew.play();
+            //pew.play();
             //MyBox.playPew = false;
+
+            //Add pewpew to array
+            MyBox.firePew();
+
         }
     }
 
