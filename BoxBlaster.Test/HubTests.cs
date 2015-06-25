2ace33b1e384c1e6f4c18d512c7c23a094929a80
sabotuer99 <sabotuer99@gmail.com>
2015-06-25 14:48:35 -0600

    Added Nunit

---
+++ BoxBlaster.Test/HubTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using NUnit;
+using NUnit.Framework;
+using BoxBlaster.Hubs;
+
+namespace BoxBlaster.Test
+{
+    public class HubTests
+    {
+        [Test]
+        public void BoxConstructorTest()
+        {
+            Box b = new Box();
+            b.color = "red";
+            b.deaths = 10;
+        }
+    }
+}
